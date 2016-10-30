e818c51b7927a6b1e7d50a1c6918a1d6d3d71122
elenagradovich <volozhinskaya@gmail.com>
2016-10-31 00:38:30 +0300

    проект в  pycharm

--- test.py
+++ test.py
@@ -10,43 +10,40 @@ def is_alert_present(wd):
     except:
         return False
 
-class test1(unittest.TestCase):
+class test(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
     
-    def test_test1(self):
+    def test_test(self):
         success = True
         wd = self.wd
         wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_id("LoginForm").click()
-        wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
+
+
+
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
         wd.find_element_by_link_text("groups").click()
         wd.find_element_by_name("new").click()
-        wd.find_element_by_name("group_name").click()
+
         wd.find_element_by_name("group_name").clear()
-        wd.find_element_by_name("group_name").send_keys("new")
-        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_name").send_keys("fghj")
+
         wd.find_element_by_name("group_header").clear()
-        wd.find_element_by_name("group_header").send_keys("dd")
-        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_header").send_keys("jjhgf")
+
         wd.find_element_by_name("group_footer").clear()
-        wd.find_element_by_name("group_footer").send_keys("cc")
+        wd.find_element_by_name("group_footer").send_keys("gfghk")
+
         wd.find_element_by_name("submit").click()
-        wd.find_element_by_link_text("group page").click()
+
         wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
+
         self.assertTrue(success)
     
     def tearDown(self):
