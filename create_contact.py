e650d58e824e7a45dda2b6b38c47405b929f003a
elenagradovich <volozhinskaya@gmail.com>
2016-11-06 19:09:51 +0300

    Работа по видео 2.2 Fixture

--- create_contact.py
+++ create_contact.py
@@ -4,6 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 import time, unittest
 from selenium.webdriver.support.ui import Select
 from contact import Contact
+import selenium.webdriver.support.ui as ui
 
 def is_alert_present(wd):
     try:
@@ -16,9 +17,9 @@ class create_contact(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
+        self.wait = ui.WebDriverWait(self.wd, 10)
     
     def test_create_contact(self):
-        success = True
         wd = self.wd
         self.home_page(wd)
         self.login(wd, user_name="admin", password="secret")
@@ -27,10 +28,8 @@ class create_contact(unittest.TestCase):
                               email="volozhinskaya@gmail.com", birth_day="2", birth_month="August",
                               birth_year="1985"))
         self.logout(wd)
-        self.assertTrue(success)
 
     def test_create_empty_contact(self):
-        success = True
         wd = self.wd
         self.home_page(wd)
         self.login(wd, user_name="admin", password="secret")
@@ -39,10 +38,20 @@ class create_contact(unittest.TestCase):
                               email="", birth_day="", birth_month="",
                               birth_year=""))
         self.logout(wd)
-        self.assertTrue(success)
 
-    def logout(self, wd):
-        wd.find_element_by_link_text("Logout").click()
+    def home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
+
+    def login(self, wd, user_name, password):
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys(user_name)
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys(password)
+        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def open_contact_page(self, wd):
+        self.wait.until(lambda driver: driver.find_element_by_link_text('add new'))
+        wd.find_element_by_link_text("add new").click()
 
     def creating_contact(self, wd, contact):
         wd.find_element_by_name("firstname").clear()
@@ -58,28 +67,13 @@ class create_contact(unittest.TestCase):
 
         wd.find_element_by_xpath("//div[@id='content']/form/select[1]").send_keys(contact.birth_day)
         wd.find_element_by_xpath("//div[@id='content']/form/select[2]").send_keys(contact.birth_month)
-        #select = Select(wd.find_element_by_xpath("//div[@id='content']/form/select[1]"))
-        #select.select_by_value("2")
-        #if not wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").is_selected():
-        #    wd.find_element_by_xpath("//div[@id='content']/form/select[1]//option[4]").click()
-        #if not wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[9]").is_selected():
-        #    wd.find_element_by_xpath("//div[@id='content']/form/select[2]//option[9]").click()
         wd.find_element_by_name("byear").clear()
         wd.find_element_by_name("byear").send_keys(contact.birth_year)
-        wd.find_element_by_xpath("//div[@id='content']/form").submit()
+        wd.find_element_by_name("submit").click()
+        self.wait.until(lambda driver: driver.find_element_by_name('searchstring'))
 
-    def open_contact_page(self, wd):
-        wd.find_element_by_link_text("add new").click()
-
-    def login(self, wd, user_name, password):
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys(user_name)
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
-
-    def home_page(self, wd):
-        wd.get("http://localhost/addressbook/")
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
 
     def tearDown(self):
         self.wd.quit()
