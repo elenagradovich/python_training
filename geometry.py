9e2155d7e1c4b3f4b2cfdf5a9e1a82474c2e4f00
elenagradovich <volozhinskaya@gmail.com>
2016-11-20 23:43:20 +0300

    Цикл for

--- geometry.py
+++ geometry.py
@@ -2,7 +2,7 @@ from geom2d.point import *
 
 
 #создаем списки
-l1 = [Point(0,3), Point(2,5), Point(1,2)]
+#l1 = [Point(0,3), Point(2,5), Point(1,2)]
 
 #l3 = sorted(l1, key=lambda p: (p.y))
 #print(l1)
@@ -10,7 +10,14 @@ l1 = [Point(0,3), Point(2,5), Point(1,2)]
 #l3 = sorted(l1, key=lambda p: (p.x))
 #print(l3, 'по x')
 # Сортировка по расстояию от центра координат Point(0,0)
-l3 = sorted(l1, key=lambda p: p.distance(Point(0,0)))
+#l3 = sorted(l1, key=lambda p: p.distance(Point(0,0)))
 
-print(l1)
-print(l3)
+#print(l1)
+#print(l3)
+
+l = [Point(i, i*i) for i in range (-5, 6)]
+
+l2 = [Point(el.x, -el.y) for el in l]
+
+print (l)
+print (l2)
--- test/test_delete.py
+++ test/test_delete.py
@@ -4,10 +4,12 @@ from model.contact import Contact
 
 def test_delete_first_group(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="test_group"))
+        app.group.create(Group(name="Dog", header="big_dog", footer="little_dog"))
     app.group.delete_first_group()
 
 def test_delete_first_contact(app):
     if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="test_contact"))
+        app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
+                 mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
+                 birth_month = "2", birth_year = "2008"))
     app.contact.delete_first_contact()
