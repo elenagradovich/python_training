403a1ff15b3befb7058aa2c3ec43dbc2ef9970c1
elenagradovich <volozhinskaya@gmail.com>
2016-11-21 08:42:40 +0300

    Map and filter

--- geometry.py
+++ geometry.py
@@ -11,13 +11,15 @@ from geom2d.point import *
 #print(l3, 'по x')
 # Сортировка по расстояию от центра координат Point(0,0)
 #l3 = sorted(l1, key=lambda p: p.distance(Point(0,0)))
-
 #print(l1)
 #print(l3)
+#l = [Point(i, i*i) for i in range (-5, 6)]
+#l2 = [Point(el.x, -el.y) for el in l]
+#l2 = list(map(lambda p: Point(p.x, -p.y), l))
+#l2 = list(filter(lambda p: p.x>0, l)) #фильтр на положительные координаты
+l = list(map(lambda i: Point(i, i*i), range(-5, 6)))
+l2 = list(filter(lambda p: p.x%2 ==0, l)) #четные координаты
 
-l = [Point(i, i*i) for i in range (-5, 6)]
-
-l2 = [Point(el.x, -el.y) for el in l]
 
 print (l)
 print (l2)
