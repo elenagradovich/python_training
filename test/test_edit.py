9906f7e0c5b3911a19a94c85fc1e72461f695817
elenagradovich <volozhinskaya@gmail.com>
2016-11-23 16:10:25 +0300

    Задание11.Реализовать проверки в виде сравнения списков

--- test/test_edit.py
+++ test/test_edit.py
@@ -34,7 +34,7 @@ def test_edit_group_header(app):
     old_groups = app.group.get_group_list()
     group = Group(header="little_dog")
     group.id = old_groups[0].id
-    app.group.edit_first(Group(group))
+    app.group.edit_first(group)
     new_groups = app.group.get_group_list()  # Новый список групп
     assert len(old_groups) == len(new_groups)
     old_groups[0] = group
@@ -47,31 +47,36 @@ def test_edit_contact(app):
         app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
                  mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
                  birth_month = "2", birth_year = "2008"))
-    old_contacts = app.contact.get_contact_list
-    app.contact.edit_first(Contact(firstname="Valya", lastname = "Petrova", address = "Minsk",
+    old_contacts = app.contact.get_contact_list()
+    contact = Contact(firstname="Valya", lastname = "Petrova", address = "Minsk",
                  mobile = "123456", email = "volozhin@gmail.com", birth_day = "7",
-                 birth_month = "11", birth_year = "2000"))
-    new_contacts = app.contact.get_contact_list
+                 birth_month = "11", birth_year = "2000")
+    contact.id = old_contacts[0].id
+    app.contact.edit_first(contact)
+    new_contacts = app.contact.get_contact_list()
     assert len(old_contacts) == len(new_contacts)
+    old_contacts[0] = contact
+    assert sorted(old_contacts, key=Group.id_or_max) == sorted(old_contacts, key=Group.id_or_max)
+
 
 #Меняем часть свойств первого контакта
-def test_edit_contact_birthday(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
-                 mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
-                 birth_month = "2", birth_year = "2008"))
-    old_contacts = app.contact.get_contact_list
-    app.contact.edit_first(Contact(birth_day="12"))
-    new_contacts = app.contact.get_contact_list
-    assert len(old_contacts) == len(new_contacts)
-
-
-def test_edit_contact_lastname(app):
-    if app.contact.count() == 0:
-        app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
-                 mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
-                 birth_month = "2", birth_year = "2008"))
-    old_contacts = app.contact.get_contact_list
-    app.contact.edit_first(Contact(lastname="Popova"))
-    new_contacts = app.contact.get_contact_list
-    assert len(old_contacts) == len(new_contacts)
+# def test_edit_contact_birthday(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
+#                  mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
+#                  birth_month = "2", birth_year = "2008"))
+#     old_contacts = app.contact.get_contact_list()
+#     app.contact.edit_first(Contact(birth_day="12"))
+#     new_contacts = app.contact.get_contact_list()
+#     assert len(old_contacts) == len(new_contacts)
+#
+#
+# def test_edit_contact_lastname(app):
+#     if app.contact.count() == 0:
+#         app.contact.create(Contact(firstname="Katya", lastname = "Ivanova", address = "Gomel",
+#                  mobile = "111111", email = "qqq@gmail.com", birth_day = "5",
+#                  birth_month = "2", birth_year = "2008"))
+#     old_contacts = app.contact.get_contact_list
+#     app.contact.edit_first(Contact(lastname="Popova"))
+#     new_contacts = app.contact.get_contact_list
+#     assert len(old_contacts) == len(new_contacts)
