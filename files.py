1a994030094208a1414cd79d2c834d66fc939849
elenagradovich <volozhinskaya@gmail.com>
2016-12-07 00:40:41 +0300

    With

--- files.py
+++ files.py
@@ -1,12 +1,10 @@
 import json
 
-f = open("c:/temp/config.json")
-try:
-    res = json.load(f)
-except ValueError as ex:
-    print(ex)
-    res = {}
-finally:
-    f.close()
+with open("c:/temp/config.json") as f:#with-обЪект будет исп-ся в рамках данного блока и зачистка не нужна
+    try:
+        res = json.load(f)
+    except ValueError as ex:
+        print(ex)
+        res = {}
 
 print(res)
