d4ac24f033a6bdb329885975c1ab8943adf35700
elenagradovich <volozhinskaya@gmail.com>
2016-12-20 14:10:06 +0300

    non-working code

--- check_db_connection.py
+++ check_db_connection.py
@@ -1,21 +1,68 @@
-from fixture.db import DbFixture
-#import pymysql.cursors
+from fixture.orm import ORMFixture
+from model.group import Group
 
-#connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
-db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
-
+db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#проверка для ORM.py
 try:
-    # cursor = connection.cursor()#создание курсора, кот. создается с помощью
-    # cursor.execute("select * from group_list")#данные возвращаемые запросом
-    # for row in cursor.fetchall():
-    #     print(row)
-    contacts = db.get_contact_list()
-    for contact in contacts:
-        print(contact)
-    print (len(contacts))
+    l=db.get_contacts_not_in_group(Group(id="338"))
+    for item in l:
+        print(item)
+    print(len(l))
 finally:
-    #connection.close()
-    db.destroy()
+    pass#db.destroy()
+
+
+
+
+# from fixture.orm import ORMFixture
+#
+# db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+# #проверка для ORM.py
+# try:
+#     l= db.get_contact_list()
+#     for item in l:
+#         print(item)
+#     print(len(l))
+# finally:
+#     pass#db.destroy()
+
+# from fixture.orm import ORMFixture
+#
+# db = ORMFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+# #проверка для ORM.py
+# try:
+#     l= db.get_group_list()
+#     for item in l:
+#         print(item)
+#     print (len(l))
+# finally:
+#     pass#db.destroy()
+
+# from fixture.db import DbFixture
+#
+# db = DbFixture(host="127.0.0.1", name="addressbook", user="root", password="")
+#
+# try:
+#     contacts = db.get_contact_list()
+#     for contact in contacts:
+#         print(contact)
+#     print (len(contacts))
+# finally:
+#     db.destroy()
+
+
+# import pymysql.cursors
+#
+# connection = pymysql.connect(host="127.0.0.1", database="addressbook", user="root", password="")
+#
+# try:
+#     cursor = connection.cursor()#создание курсора, кот. создается с помощью
+#     cursor.execute("select * from group_list")#данные возвращаемые запросом
+#     for row in cursor.fetchall():
+#         print(row)
+# finally:
+#     connection.close()
+
 
 
 
---
+++ fixture/orm.py
@@ -0,0 +1,69 @@
+from pony.orm import *
+from datetime import datetime
+from model.group import Group
+from model.contact import Contact
+from pymysql.converters import decoders
+
+class ORMFixture:
+
+    db = Database()#объект ,на основании которого делается привязка
+
+    class ORMGroup(db.Entity):#этоткласс описывает объекты, кот. будут сохраняться в БД
+        _table_ = 'group_list'#название таблицы
+        id = PrimaryKey(int, column='group_id')#ключ-обязательное поле по кот. объекты инициализируются
+        name = Optional(str, column='group_name')
+        header = Optional(str, column='group_header')
+        footer = Optional(str, column='group_footer')
+        contacts = Set(lambda: ORMFixture.ORMContact, table="address_in_groups", column="id", reverse="groups", lazy=True) #set множество, lambda здесь возвращает множество
+
+    class ORMContact(db.Entity):
+        _table_ = 'addressbook'
+        id = PrimaryKey(int, column='id')
+        lastname = Optional(str, column='lastname')
+        firstname = Optional(str, column='firstname')
+        deprecated = Optional(datetime, column='deprecated')
+        groups = Set(lambda: ORMFixture.ORMGroup, table="address_in_groups", column="group_id", reverse="contacts", lazy=True)
+
+    def __init__(self, host, name, user, password):#в качестве параметров принимает набор, что и DBFixtureпривязка к БД
+        self.db.bind('mysql', host=host, database=name,
+                     user=user, password=password, conv=decoders)
+        self.db.generate_mapping()#сопоставление свойств классов с полями таблицы
+        sql_debug(True)
+
+    def convert_groups_to_model(self, groups):#преобразование ORMобъекта, в объект типа Group
+        def convert(group):
+            return Group(id=str(group.id), name=group.name,
+                         header=group.header, footer=group.footer)
+        return (list(map(convert, groups)))
+
+    @db_session#пометка, что след блок кода выполняется в рамках этой сессии
+    def get_group_list(self):# реализация функций, получающие списки объектов
+        return self.convert_groups_to_model(select(g for g in ORMFixture.ORMGroup))
+        #return list(select(g for g in ORMFixture.ORMGroup))#выборка из объектов данного класса
+
+    def convert_contacts_to_model(self, contacts):#преобразование ORMобъекта, в объект типа Group
+        def convert(contact):
+            return Contact(id=str(contact.id), firstname=contact.firstname,lastname=contact.lastname)
+        return (list(map(convert,contacts)))
+
+
+    @db_session  # пометка, что след блок кода выполняется в рамках этой сессии
+    def get_contact_list(self):  # реализация функций, получающие списки объектов
+        return self.convert_contacts_to_model\
+            (select(c for c in ORMFixture.ORMContact if c.deprecated is None))#if выбрать такие контакты которые None,нулевые даты преобразовываются в None
+                                                                              # извлечение всех контактов
+
+    #метод получения контактов, входящих в группу
+    @db_session
+    def get_contacts_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]#выбираем группу сзаданным идентификатором, тк это список выбираем по индексу
+        return self.convert_contacts_to_model(orm_group.contacts)#преобразуем объекты типа ORM в model
+
+    #метод получения списка контактов, не входящих в заданную группу
+    @db_session
+    def get_contacts_not_in_group(self, group):
+        orm_group = list(select(g for g in ORMFixture.ORMGroup if g.id == group.id))[0]#выбираем группу сзаданным идентификатором, тк это список выбираем по индексу
+        return self.convert_contacts_to_model \
+            (select(c for c in ORMFixture.ORMContact if c.deprecated is None
+                    and orm_group not in c.groups))#извлечение всех контактов с условием
+
